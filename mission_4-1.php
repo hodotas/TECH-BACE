46b0fdfa45fb3766ba9fd3084b85ab50795bf2cf
hodotas <71540447+hodotas@users.noreply.github.com>
2020-09-21 14:14:51 +0900

    Add files via upload

---
+++ mission_4-1.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang = "ja">
+    <head>
+        <meta charset = "UTF-8">
+        <title>mission4-1</title>
+    </head>
+    <body>
+        <?php
+        
+	    $dsn = 'データベース名';
+    	$user = 'ユーザー名';
+	    $password = 'パスワード';
+    	$pdo = new PDO($dsn, $user, $password, array(PDO::ATTR_ERRMODE => PDO::ERRMODE_WARNING));
+        ?>
+    </body>
+</html>
